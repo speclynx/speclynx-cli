300344cb9e053b388a7829e09014d93014272334
Vladimír Gorej <vladimir.gorej@gmail.com>
2026-07-20 10:39:47 +0100

    chore(deps): migrate to babel 8 (#96)

--- babel.config.cjs
+++ babel.config.cjs
@@ -11,29 +11,20 @@ module.exports = {
           {
             debug: false,
             modules: 'commonjs',
-            loose: true,
-            useBuiltIns: false,
             forceAllTransforms: false,
             ignoreBrowserslistConfig: false,
             exclude: ['transform-function-name'],
           },
         ],
-        [
-          '@babel/preset-typescript',
-          {
-            allowDeclareFields: true,
-          },
-        ],
+        ['@babel/preset-typescript', { onlyRemoveTypeImports: false }],
       ],
       plugins: [
         [
           '@babel/plugin-transform-runtime',
           {
-            corejs: { version: 3, proposals: false },
+            moduleName: '@babel/runtime-corejs3',
             absoluteRuntime: false,
-            helpers: true,
-            regenerator: false,
-            version: '^7.22.15',
+            version: '^8.0.0',
           },
         ],
         process.env.NODE_ENV !== 'test'
@@ -52,29 +43,20 @@ module.exports = {
           {
             debug: false,
             modules: false,
-            useBuiltIns: false,
             forceAllTransforms: false,
             ignoreBrowserslistConfig: false,
             exclude: ['transform-function-name'],
           },
         ],
-        [
-          '@babel/preset-typescript',
-          {
-            allowDeclareFields: true,
-          },
-        ],
+        ['@babel/preset-typescript', { onlyRemoveTypeImports: false }],
       ],
       plugins: [
         [
           '@babel/plugin-transform-runtime',
           {
-            corejs: { version: 3, proposals: false },
+            moduleName: '@babel/runtime-corejs3',
             absoluteRuntime: false,
-            helpers: true,
-            regenerator: false,
-            useESModules: true,
-            version: '^7.22.15',
+            version: '^8.0.0',
           },
         ],
         [
